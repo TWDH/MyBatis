5ad8ecff093666e77527b8945c7fc16871affed2
TWDH <hezhukevin@gmail.com>
2020-12-03 14:50:25 -0500

    first

---
+++ day02_01CRUD/src/test/java/com/itheima/test/MybatisTest.java
@@ -0,0 +1,65 @@
+package com.itheima.test;
+
+import com.itheima.dao.IUserDao;
+import com.itheima.domain.User;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.InputStream;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * @author 黑马程序员
+ * @Company http://www.ithiema.com
+ * mybatis的入门案例
+ */
+public class MybatisTest {
+    private InputStream in;
+    private SqlSession sqlSession;
+    private IUserDao userDao;
+
+
+    @Before
+    public void init() throws Exception{
+        //1.读取配置文件
+        in = Resources.getResourceAsStream("SqlMapConfig.xml");
+        //2.创建SqlSessionFactory工厂
+        SqlSessionFactory factory = new SqlSessionFactoryBuilder().build(in);
+        //3.使用工厂生产SqlSession对象
+        sqlSession = factory.openSession();
+        //4.使用SqlSession创建Dao接口的代理对象
+        userDao = sqlSession.getMapper(IUserDao.class);
+    }
+    @After
+    public void destroy() throws Exception{
+        sqlSession.commit();
+        sqlSession.close();
+        in.close();
+    }
+
+    @Test
+    public void testFindAll(){
+        //5.使用代理对象执行方法
+        List<User> users = userDao.findAll();
+        for(User user : users){
+            System.out.println(user);
+        }
+    }
+
+    @Test
+    public void testSaveUser(){
+        User user = new User();
+        user.setUsername("张三");
+        user.setAddress("新文化街");
+        user.setSex("m");
+        user.setBirthday(new Date());
+
+        userDao.saveUser(user);
+    }
+}
