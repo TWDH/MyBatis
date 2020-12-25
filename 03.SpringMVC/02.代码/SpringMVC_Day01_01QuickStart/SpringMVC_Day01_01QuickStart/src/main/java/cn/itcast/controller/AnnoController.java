7890321058b9d2aa2e53c96cb97d1d658856b34f
TWDH <hezhukevin@gmail.com>
2020-12-25 14:25:00 -0500

    update

--- 03.SpringMVC/02.代码/SpringMVC_Day01_01QuickStart/SpringMVC_Day01_01QuickStart/src/main/java/cn/itcast/controller/AnnoController.java
+++ 03.SpringMVC/02.代码/SpringMVC_Day01_01QuickStart/SpringMVC_Day01_01QuickStart/src/main/java/cn/itcast/controller/AnnoController.java
@@ -1,10 +1,11 @@
 package cn.itcast.controller;
 
+import cn.itcast.domain.User;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Date;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/anno")
@@ -25,5 +26,50 @@ public class AnnoController {
     public String testPathVariable(@PathVariable(name = "sid") String id) {
         System.out.println("sid: "+ id);
         return "success";
+    }
+
+    @RequestMapping("/testRequestHeader")
+    public String testRequestHeader(@RequestHeader(value = "Accept") String header) {
+        System.out.println("header: "+ header);
+        return "success";
+    }
+
+    @RequestMapping("/testCookieValue")
+    public String testCookieValue(@CookieValue(value = "JSESSIONID") String CookieValue) {
+        System.out.println("CookieValue: "+ CookieValue);
+        return "success";
+    }
+
+    /**
+     * ModelAttribute
+     * @param user
+     * @return
+     */
+    @RequestMapping("/testModelAttribute")
+    public String testModelAttribute(@ModelAttribute("key") User user) {
+        System.out.println("ModelAttribute: "+ user);
+        return "success";
+    }
+
+    /*@ModelAttribute
+    public User showUser(String uname) {
+        System.out.println("showUser执行了");
+        //模拟用户查询数据库
+        User user = new User();
+        user.setUname(uname);
+        user.setAge(20);
+        user.setDate(new Date());
+        return user;
+    }*/
+
+    @ModelAttribute
+    public void showUser(String uname, Map<String, User> map) {
+        System.out.println("showUser执行了");
+        //模拟用户查询数据库
+        User user = new User();
+        user.setUname(uname);
+        user.setAge(20);
+        user.setDate(new Date());
+        map.put("key", user);
     }
 }
--- 03.SpringMVC/02.代码/SpringMVC_Day01_01QuickStart/SpringMVC_Day01_01QuickStart/src/main/webapp/anno.jsp
+++ 03.SpringMVC/02.代码/SpringMVC_Day01_01QuickStart/SpringMVC_Day01_01QuickStart/src/main/webapp/anno.jsp
@@ -21,8 +21,22 @@
     用户年龄：<input type="text" name="age"/><br/>
     <input type="submit" value="提交"/>
 </form>
+<br>
 
 <a href="anno/testPathVariable/10">testPathVariable</a>
+<br>
+
+<a href="anno/testRequestHeader">testRequestHeader</a>
+<br>
+
+<a href="anno/testCookieValue">testCookieValue</a>
+<br>
+
+<form action="anno/testModelAttribute" method="post">
+    用户姓名：<input type="text" name="uname"/><br/>
+    用户年龄：<input type="text" name="age"/><br/>
+    <input type="submit" value="提交"/>
+</form>
 
 </body>
 </html>
